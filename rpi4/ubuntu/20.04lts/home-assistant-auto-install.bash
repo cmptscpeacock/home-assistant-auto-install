54bd0cc545e05e1c2d8219d0859174c1b994b8f4
CHRIS PEACOCK <chris.peacock@cmptechsol.co.uk>
2021-11-30 14:35:32 +0000

    formatted

--- rpi4/ubuntu/20.04lts/home-assistant-auto-install.bash
+++ rpi4/ubuntu/20.04lts/home-assistant-auto-install.bash
@@ -23,7 +23,7 @@ RESETUNDERLINE='\033[24m'
 
 # create local file to prevent prompts when used with apt upgrade
 
-wget -O local https://raw.githubusercontent.com/cmptscpeacock/home-assistant-auto-install/master/local
+wget -O local https://raw.githubusercontent.com/cmptscpeacock/home-assistant-auto-install/master/rpi4/ubuntu/20.04lts/files/local
 sudo mv local /etc/apt/apt.conf.d/local
 
 ## Perform apt update
@@ -48,40 +48,11 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io bash jq curl avahi-daemon dbus
 
+# install home assistant supervisor
 
 sudo su
 sudo curl -sL https://raw.githubusercontent.com/cmptscpeacock/home-assistant-auto-install/master/rpi4/ubuntu/20.04lts/installer.sh | bash -s -- -m raspberrypi4-64
 
-# install various utilities
-
-sudo -i apt install -y python3-dev python3-pip python3-venv git ffmpeg nginx nmap cmake build-essential libssl-dev libffi-dev python-dev python3-testresources
-sudo -i apt install -y python3-dev python3-pip python3-venv git ffmpeg nginx nmap cmake build-essential libssl-dev libffi-dev python-dev python3-testresources
-
-# install setuptools and upgrade pip
-
-sudo -i -H pip install setuptools
-sudo -i -H pip install --upgrade pip
-
-# change directory
-
-cd /home/homeassistant
-
-# install home assistant
-
-sudo python3.8 -m venv homeassistant
-cd homeassistant
-source bin/activate
-#sudo pip install --upgrade pip
-sudo python3 -m pip install wheel
-sudo pip3 install homeassistant --ignore-installed PyYAML
-#sudo python3 -m pip install homeassistant --ignore-installed PyYAML
-
-# setup home assistant service
-
-wget -O home-assistant@homeassistant https://raw.githubusercontent.com/cmptscpeacock/home-assistant-auto-install/master/home-assistantAThomeassistant.service
-sudo mv home-assistant@homeassistant /etc/systemd/system/home-assistant@homeassistant.service
-sudo -i systemctl enable home-assistant@homeassistant
-
 ## output details
 
-printf "\n${RED}${UNDERLINE}Home Assistant URL:${WHITE}${RESETUNDERLINE}  http://1.1.1.1:8123 \n\n"
+printf "\n${RED}${UNDERLINE}Home Assistant URL:${WHITE}${RESETUNDERLINE}  http://x.x.x.x:8123 \n\n"
